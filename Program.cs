1b25b7feddab83089e98deb43829d532fdb8f65c
oz-mollerstuen <mollerstuen.works@gmail.com>
2022-12-05 08:54:36 -0800

    Add all files for baisic ping pong application
    Co-authored-by: Yoonis Ali <yoonismahamoudali@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+public class Program 
+{
+    public static void Main()
+    {
+        Console.WriteLine("Girl can I get your number please!!!");
+        string stringUserInput = Console.ReadLine();
+        int userInput = int.Parse(stringUserInput);
+        for (int index = 1; index <= userInput; index++) 
+        {
+            if (index % 3 == 0 && index % 5 == 0) 
+            {
+                Console.WriteLine("Ping-Ping!!!");
+            } 
+            else if (index % 3 == 0)
+            {
+                Console.WriteLine("Ping");
+            }
+            else if (index % 5 == 0)
+            {
+                Console.WriteLine("Pong");
+            }
+            else
+            {
+                Console.WriteLine(index);
+            }
+        }
+    }
+
+}
